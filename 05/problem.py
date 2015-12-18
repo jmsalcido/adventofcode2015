2ad5f1d1e3b5a4840c4a16bb4dcf1cf95d6875fa
jmsalcido <eljose152@gmail.com>
2015-12-18 14:38:45 -0700

    add problem 5, still missing some rules

---
+++ 05/problem.py
@@ -0,0 +1,94 @@
+def do():
+    # problem_second(get_input())
+    test = [
+        # "qjhvhtzxzqqjkmpb",
+        # "xxyxx",
+        # "uurcxstgmygtbstg",
+        # "ieodomkazucvgmuy",
+        "rxexcbwhiywwwwnu"
+    ]
+    problem_second(test)
+
+
+def problem_first(iterable):
+    vowels = ["a", "e", "i", "o", "u"]
+    forbidden_words = ["ab", "cd", "pq", "xy"]
+    nice_strings = 0
+    for line in iterable:
+        if len(line) < 1:
+            continue
+        # Lets say all words are nice at first until proven nasty.
+        nice_string = True
+        vowels_count = []
+        repeated_letter = False
+        for word in forbidden_words:
+            if line.find(word) != -1:
+                nice_string = False
+        if not nice_string:
+            continue
+        for char in line:
+            if repeated_letter == char:
+                repeated_letter = True
+            if repeated_letter != True:
+                repeated_letter = char
+            if char in vowels:
+                vowels_count.append(char)
+        if len(vowels_count) < 3 or repeated_letter != True:
+            continue
+        nice_strings += 1
+    print(nice_strings)
+
+
+def problem_second(iterable):
+    nice_strings = 0
+    # check case "aaa" or "bbb" or "ccc" or "xxx"
+    nice_dict = {}
+    nice_set = set()
+    for line in iterable:
+        line = line.replace("\n", "")
+        word_dict = {}
+        len_line = len(line)
+        for x in range(0,len_line):
+            if (x+1) > len_line:
+                break
+            import pdb; pdb.set_trace()
+            # issue on asdwwwwwords
+            if (x+2) < len_line and line[x] == line[x+1] and line[x] == line[x+2]:
+                continue
+            key = line[x:x+2]
+            if len(key) != 2:
+                break
+            if key in word_dict:
+                word_dict[key] += 1
+            else:
+                word_dict[key] = 1
+        repeated_pair = False
+        char_between_repeated = False
+        repeated_pair_str = ""
+        for x in range(0, len_line):
+            # import pdb; pdb.set_trace()
+            word = line[x:x+2]
+            if repeated_pair and char_between_repeated:
+                break
+            if not repeated_pair and len(word) == 2:
+                repeated_pair = word_dict[word] >= 2
+                if repeated_pair:
+                    repeated_pair_str = word
+            if not char_between_repeated:
+                char_between_repeated = checkCharBetweenPair(line[x - 1:x + 2])
+        import pdb; pdb.set_trace()
+        if repeated_pair and char_between_repeated:
+            nice_set.add(line)
+        nice_dict[line] = {"word":repeated_pair_str, "char_between_repeated": char_between_repeated, "repeated_pair": repeated_pair}
+    print(len(nice_set))
+    return nice_dict
+
+
+def checkCharBetweenPair(word):
+    if len(word) != 3:
+        return False
+    return word[0] == word[2]
+
+
+def get_input():
+    return open("input", "r")
